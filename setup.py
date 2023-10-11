20dd5ff0999b6b454248063239a025296939f950
filispeen <72784386+filispeen@users.noreply.github.com>
2023-10-11 09:56:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='F_Discord_webhook',
     packages=find_packages(),
-    version='2.0.10',
+    version='2.0.15',
     description='Discord Webhook Messanger',
     author='FILISPEEN',
     install_requires=['discord', 'pytz', 'aiohttp', 'argparse', 'watchdog'],
