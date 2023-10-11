976ac89fbb265d0ea92286a0cf9a73334f7d81cb
filispeen <72784386+filispeen@users.noreply.github.com>
2023-10-11 22:51:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ For detailed usage instructions, please refer to the documentation or the projec
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License,   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
   ],
