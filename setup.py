60a4fcf053112d32d3b195715989900cd9c65e4c
filispeen <72784386+filispeen@users.noreply.github.com>
2023-10-12 10:03:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='svc_ds_webhook',
     packages=find_packages(),
-    version='2.0.20',
+    version='2.0.21',
     description='Discord Webhook Messanger',
     license='MIT',
     author='FILISPEEN',
